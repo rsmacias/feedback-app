4f2e815d6387480c878646225c6333add4079b7d
Robert Macías <rsmacias@gmail.com>
2022-06-04 00:46:46 -0500

    1.- Installation and integration of react-icons 2.- Add remove feedback feature with drop up event handler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 
 function App () {
+
+    const [feedback, setFeedback] = useState(FeedbackData);
+
+    const deleteFeedback = (id) => {
+        if(window.confirm(`Are your sure you want delete the ${id} comment?`)){
+            console.log('App', id);
+            setFeedback(feedback.filter((item) => item.id !== id));
+        }
+    }; 
+
     return (
         <>
             <Header />
             <div className="container">
                 <h1>This is my App</h1>
-                <FeedbackList feedback={FeedbackData} />
+                <FeedbackList feedback={feedback} 
+                              handleDelete={deleteFeedback} />
             </div>
         </>
     );
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,21 @@
+import { FaTimes } from 'react-icons/fa';
 import Card from './shared/Card';
 import PropTypes from 'prop-types';
 
-function FeedbackItem ({item}) {
+function FeedbackItem ({item, handleDelete}) {
+
+    const handleClick = (id) => {
+        console.log(`Todo: remove feedback with id ${id}`);
+        handleDelete(id);
+    };
 
     return (
         <Card>
             <div className="num-display">{item.rating}</div>
+            <button className='close'
+                    onClick={() => handleClick(item.id)} >
+                <FaTimes color='purple' />
+            </button>
             <div className="text-display">
                 {item.text}
             </div>
