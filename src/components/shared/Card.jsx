af99d8b63e1617a78b5d2dd858feccc7fcc767a4
Robert Macías <rsmacias@gmail.com>
2022-06-03 23:48:09 -0500

    Version with conditional styles

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,6 +1,11 @@
 function Card({children, reverse}) {
-  return (
-    <div className={`card ${ reverse && 'reverse'}`}>
+    const cardStyles = {
+        backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+        color: reverse ? '#fff' : '#000'
+    }
+
+    return (
+    <div className='card' style={cardStyles}>
         {children}
     </div>
   )
