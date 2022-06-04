f79ba48587d6c0a5c3aeae9620ef37107b2fc61b
Robert Macías <rsmacias@gmail.com>
2022-06-04 14:06:15 -0500

    Creating and integrating a custom button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Card from './shared/Card';
+import Button from './shared/Button';
 
 function FeedbackForm() {
 
@@ -19,7 +20,7 @@ function FeedbackForm() {
                     placeholder='Write a review'
                     value={text}
                     onChange={handleOnChange} />
-                <button type="submit">Send</button>
+                <Button type="submit">Send</Button>
             </div>            
         </form>
     </Card>
